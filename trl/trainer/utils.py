22c92e69086ad6491be9724992dccacc793cdb31
wenxindongwork <wenxindong@google.com>
2024-08-20 15:27:30 -0700

    Reapply "Merge branch 'huggingface:main' into mark_steps"
    This reverts commit 8b364df12d5f6fff3fb1f70b1078985706c5c491.

--- trl/trainer/utils.py
+++ trl/trainer/utils.py
@@ -811,12 +811,13 @@ def get_peft_config(model_config: ModelConfig) -> "Optional[PeftConfig]":
         )
 
     peft_config = LoraConfig(
+        task_type=model_config.lora_task_type,
         r=model_config.lora_r,
+        target_modules=model_config.lora_target_modules,
         lora_alpha=model_config.lora_alpha,
         lora_dropout=model_config.lora_dropout,
         bias="none",
-        task_type=model_config.lora_task_type,
-        target_modules=model_config.lora_target_modules,
+        use_rslora=model_config.use_rslora,
         modules_to_save=model_config.lora_modules_to_save,
     )
 
@@ -879,6 +880,7 @@ class OnPolicyConfig(TrainingArguments):
     """a unique name of this run"""
     sanity_check: bool = False
     """wether to run in debug mode"""
+    dataset_num_proc: Optional[int] = None
 
     # batch size related config
     num_mini_batches: int = 1
